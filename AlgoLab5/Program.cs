2c8994fadfd570ed3e984bdb64e9ef42b5a002cf
Sexualizer <alexander.varfolomei@gmail.com>
2022-05-22 19:59:17 +0300

    Init2

--- AlgoLab5/Program.cs
+++ AlgoLab5/Program.cs
@@ -8,76 +8,70 @@ namespace AlgoLab5
 {
     public static class Program
     {
-        public static void Sort()
+        public static void Sort(int n)
         {
-            string f1 = "f0.txt";
-            string f2 = "f1.txt";
-            string f3 = "f2.txt";
-            bool end = false;
-
-            Sequence s1 = new Sequence();
-            Sequence s2 = new Sequence();
-            Sequence s3 = new Sequence();
-            List<Sequence> list = new List<Sequence>() {s1, s2, s3};
-
+            List<Sequence> list = new List<Sequence>(n);
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(new Sequence());
+            }
             do
             {
                 DistributionPhase(list);
                 MergePhase(list);
-            } while (s1.CountOfSeries != 1);
+            } while (list[0].CountOfSeries != 1);
         }
 
         public static void DistributionPhase(List<Sequence> list)
         {
             string f1 = "f0.txt";
-            string f2 = "f1.txt";
-            string f3 = "f2.txt";
-            
-            
-            list[0].StartRead(f1);   
-            list[1].StartWrite(f2);
-            list[2].StartWrite(f3);
-            list[1].SizeOfSeries =  list[0].SizeOfSeries;
-            list[2].SizeOfSeries =   list[0].SizeOfSeries;
-            while (!  list[0].Eof)
+            list[0].StartRead(f1);
+            for (int i = 1; i < list.Count; i++)
             {
-                list[1].StartCopy(  list[0]);
-                list[2].StartCopy(  list[0]);
+                list[i].StartWrite("f" + i + ".txt");
+                list[i].SizeOfSeries = list[0].SizeOfSeries;
+            }
+            while (!list[0].Eof)
+            {
+                for (int i = 1; i < list.Count; i++)
+                {
+                    list[i].StartCopy(list[0]);
+                }
             }
             
             list[0].CloseReader();
-            list[1].CloseWriter();
-            list[2].CloseWriter();
+            for (int i = 1; i < list.Count; i++)
+            {
+                list[i].CloseWriter();
+            }
         }
 
         public static void MergePhase(List<Sequence> list)
         {
             string f1 = "f0.txt";
-            string f2 = "f1.txt";
-            string f3 = "f2.txt";
-            bool end = false;
-
-           
-            
-            list[0].StartWrite(f1);   
-            list[1].StartRead(f2);
-            list[2].StartRead(f3);
+            list[0].StartWrite(f1);
+            for (int i = 1; i < list.Count; i++)
+            {
+                list[i].StartRead("f" + i + ".txt");
+            }
             
             while (!list[1].Eof)
             {
-                list[0].Merge(new List<Sequence>(){ list[1],list[2]});
+                list[0].Merge(list.Skip(1).ToList());
             }
 
             list[0].SizeOfSeries *= 2;
             list[0].CloseWriter();
-            list[1].CloseReader();
-            list[2].CloseReader();
+            for (int i = 1; i < list.Count; i++)
+            {
+                list[i].CloseReader();
+            }
         }
         
 
         public static void Main()
         {
-           Sort();
+           Sort(6);
         }
     }
 }
